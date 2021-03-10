eff4d618e5fea0e6b04f808864027e5431564f4a
alherd <bykalgerd@gmail.com>
2021-03-10 09:37:29 +0300

    Refactor of validation logic

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -7,15 +7,15 @@ describe Validator do
   let(:incorrect_file_path) { 'spec/fixtures/unexisting_file.log' }
   let(:empty_test_file) { 'spec/fixtures/test_empty.log' }
 
-  it 'correct' do
+  it 'checks correct validation' do
     expect(Validator.file_suitable?(correct_test_log)).to eq(true)
   end
 
-  it 'empty' do
+  it 'checks validation of empty file' do
     expect(Validator.file_suitable?(empty_test_file)).to eq(false)
   end
 
-  it 'incorrect path file' do
+  it 'checks validation of incorrect path file' do
     expect(Validator.file_suitable?(incorrect_file_path)).to eq(false)
   end
 end
