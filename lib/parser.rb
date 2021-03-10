37e9e03041fea58a073ff955fc451d06f39f2d60
alherd <bykalgerd@gmail.com>
2021-03-10 11:01:22 +0300

    create logic for parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,9 +4,45 @@ require './lib/data_structure'
 
 # This class parses array of entries to array with visits
 class Parser
+  PAGE_VISITS = 'visits'
+  PAGE_UNIQ_VIEWS = 'unique views'
+
   def initialize
+    @visits = []
   end
 
   def parse(array, parse_type)
+    load_visits(array) if @visits.empty?
+    visits_grouped_by_urls ||= group_visits_by_urls
+
+    map_visits_by_parse_type(visits_grouped_by_urls, parse_type)
+  end
+
+  private
+
+  def load_visits(array)
+    array.each do |entry|
+      splitted_string = entry.split(' ')
+      next unless splitted_string.count == 2
+
+      url, ip = splitted_string
+      @visits << DataStructure.new(url: url, ip: ip)
+    end
+  end
+
+  def map_visits_by_parse_type(visits_grouped_by_urls, parse_type)
+    case parse_type
+    when PAGE_VISITS
+      visits_grouped_by_urls.map { |url, visits| [url, visits.count] }
+    when PAGE_UNIQ_VIEWS
+      visits_grouped_by_urls.map { |url, visits| [url, visits.map(&:ip).uniq.count] }
+    else
+      puts 'Unknown parse type'
+      []
+    end
+  end
+
+  def group_visits_by_urls
+    @visits.group_by(&:url)
   end
 end
