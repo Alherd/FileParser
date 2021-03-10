6ceb2ac40918a9de33a4024e7c4a38b67b5664b0
alherd <bykalgerd@gmail.com>
2021-03-10 09:36:08 +0300

    Create validation logic

--- lib/validator.rb
+++ lib/validator.rb
@@ -3,5 +3,17 @@
 # This Validator checks file for existing and emptiness
 class Validator
   def self.file_suitable?(file_path)
+    is_suitable = true
+    unless File.exist?(file_path)
+      puts 'Log does not exist'
+      is_suitable = false
+    end
+
+    if File.empty?(file_path)
+      puts 'Log is empty'
+      is_suitable = false
+    end
+
+    is_suitable
   end
 end
