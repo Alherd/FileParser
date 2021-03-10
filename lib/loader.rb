be13acd760f5175642b772df7dc00cb522478d7b
alherd <bykalgerd@gmail.com>
2021-03-10 10:00:43 +0300

    create loader logic

--- lib/loader.rb
+++ lib/loader.rb
@@ -3,5 +3,8 @@
 # This class loads entries from file path
 class Loader
   def load(file_path)
+    return [] unless Validator.file_suitable?(file_path)
+
+    IO.foreach(file_path).lazy
   end
 end
