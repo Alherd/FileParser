e5f10ccc92eca3928d6116c558442081e8092042
alherd <bykalgerd@gmail.com>
2021-03-10 11:19:24 +0300

    create integration test

---
+++ lib/manager.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This class manages process of parsing and output file
+class Manager
+  def show_page_visits
+  end
+
+  def show_pages_uniq_views
+  end
+
+  private
+
+  def handle(parse_type, header)
+  end
+end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require './lib/manager'
+
+describe 'Integration test' do
+  let(:test_log_path) { 'spec/fixtures/test.log' }
+  let(:result_page_visits) do
+    "\nList of webpages with most page views ordered from most pages views to less page views\n" \
+    "/help_page/1 6 visits \n/home 3 visits \n/about/2 3 visits \n" \
+    "/index 3 visits \n/contact 3 visits \n/about 2 visits \n"
+  end
+  let(:result_page_uniq_views) do
+    "\nList of webpages with most unique page views\n" \
+    "/help_page/1 5 unique views \n/home 3 unique views \n/index 3 unique views \n" \
+    "/about/2 2 unique views \n/about 2 unique views \n/contact 2 unique views \n"
+  end
+  let(:manager) { Manager.new([test_log_path]) }
+
+  it 'checks output page visits' do
+    expect { manager.show_page_visits }.to output(result_page_visits).to_stdout
+  end
+
+  it 'checks output page unique views' do
+    expect { manager.show_pages_uniq_views }.to output(result_page_uniq_views).to_stdout
+  end
+end
