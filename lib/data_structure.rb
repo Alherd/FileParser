9354e32244bfc593037316ce6309127626ddf726
alherd <bykalgerd@gmail.com>
2021-03-10 09:20:24 +0300

    Create data struct model

--- lib/data_structure.rb
+++ lib/data_structure.rb
@@ -2,6 +2,10 @@
 
 # This class defines structure of file entry
 class DataStructure
-  def initialize
+  attr_reader :url, :ip
+
+  def initialize(url:, ip:)
+    @url = url
+    @ip = ip
   end
 end
--- spec/data_structure_spec.rb
+++ spec/data_structure_spec.rb
@@ -4,14 +4,14 @@ require './lib/data_structure'
 
 describe DataStructure do
   it 'checks url field of data structure' do
-    log_visit = described_class.new(url: '/page', ip: '0.0.0.0')
+    data_structure = described_class.new(url: '/page', ip: '0.0.0.0')
 
-    expect(log_visit.url).to eq('/page')
+    expect(data_structure.url).to eq('/page')
   end
 
   it 'checks ip field of data structure' do
-    log_visit = described_class.new(url: '/page', ip: '0.0.0.0')
+    data_structure = described_class.new(url: '/page', ip: '0.0.0.0')
 
-    expect(log_visit.ip).to eq('0.0.0.0')
+    expect(data_structure.ip).to eq('0.0.0.0')
   end
 end
