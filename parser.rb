10906eca9cf20defd0f05b0667e76f5aa256f199
alherd <bykalgerd@gmail.com>
2021-03-12 17:23:02 +0300

    clean code

--- parser.rb
+++ parser.rb
@@ -2,7 +2,7 @@
 
 require './lib/log_manager'
 
-manager = LogManager.new(ARGV)
+log_manager = LogManager.new(ARGV)
 
-manager.show_page_visits
-manager.show_pages_uniq_views
+log_manager.show_page_visits
+log_manager.show_pages_uniq_views
